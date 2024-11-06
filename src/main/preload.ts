acd7f7febafeefc7b0776d640425de896ea49a8b
Moskwa Quentin moskwaquentin@gmail.com <95235691+QuentinMoskwa@users.noreply.github.com>
2024-11-06 11:36:27 +0100

    feat: ajout du channel get-system-info dans l'ipc.

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -2,7 +2,8 @@
 /* eslint no-unused-vars: off */
 import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron';
 
-export type Channels = 'ipc-example';
+// Définir les types de canaux
+export type Channels = 'ipc-example' | 'get-system-info';
 
 const electronHandler = {
   ipcRenderer: {
@@ -20,6 +21,9 @@ const electronHandler = {
     },
     once(channel: Channels, func: (...args: unknown[]) => void) {
       ipcRenderer.once(channel, (_event, ...args) => func(...args));
+    },
+    async getSystemInfo() {
+      return await ipcRenderer.invoke('get-system-info');
     },
   },
 };
